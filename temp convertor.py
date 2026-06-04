4857251de3cffcad99d96ee8e17617191ef780ab
Niharika Kamble <niharikakamble98@gmail.com>
2026-06-05 00:26:39 +0530

    Add files via upload

---
+++ temp convertor.py
@@ -0,0 +1,5 @@
+celcius = float (input ("enter the temp in celcius:"))
+fahren=(celcius*9/5)+32
+print("temp in fahrenheit is ",fahren)
+kelvin=(celcius + 273.15)
+print("temp in kelvin is ",kelvin)
